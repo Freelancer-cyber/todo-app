0d384698168b50b57e4223b3d6259431adb955dd
Freelancer-cyber <65244886+Freelancer-cyber@users.noreply.github.com>
2020-05-26 19:24:37 +0300

    Create div with app and add new function for add new task

---
+++ todo_app.js
@@ -0,0 +1,20 @@
+// Aplication Todo
+const app_todo = new Vue(
+  {
+    //Element
+    el: '#todo_app',
+
+    // Data
+    data: {
+      todo_title: 'Todo list',
+      newTodo: '',
+    },
+
+    //Methods
+    methods: {
+      addTodo() {
+
+      },
+    },
+  }
+);
