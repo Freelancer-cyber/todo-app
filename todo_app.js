b88611963d5e639a35beb5079e5d5f3cbcf4ad6f
Freelancer-cyber <65244886+Freelancer-cyber@users.noreply.github.com>
2020-05-28 19:12:27 +0300

    ver 1 app

--- todo_app.js
+++ todo_app.js
@@ -1,6 +1,5 @@
 // Aplication Todo
-const app_todo = new Vue(
-  {
+const app_todo = new Vue({
     //Element
     el: '#todo_app',
 
@@ -8,13 +7,56 @@ const app_todo = new Vue(
     data: {
       todo_title: 'Todo list',
       newTodo: '',
+      newGroup:'',
+      todoId: 0, // Variabila asta !!!!!!!!!!!!!!!!!
+      groupsTodos: [],
+      todos: [],
+
     },
 
     //Methods
     methods: {
+      // Function - add new group
+      addGroup() {
+        this.todos.splice(0,this.todos.length);
+        this.groupsTodos.push({
+          title: this.newGroup,
+          completed: false,
+        });
+      },
+      // Function - add new task
       addTodo() {
+        if (this.newTodo.length > 22 ){
+          this.newTodo = '';
+        };
+        this.todos.push({
+          title: this.newTodo,
+          completed: false,
+        });
+        this.todoId++;
+        this.newTodo = '';
+      },
+
+      // Function - remove a task
+      removeTodo(todo){
+        const indexTodo = this.todos.indexOf(todo);
+        this.todos.splice(indexTodo,1);
+      },
+
+      // Function - remove all tasks
+      allRemove() {
+        this.todos.splice(0,this.todos.length);
+      },
+
+      // Function - all task mark as done
+      allDone() {
+        this.todos.forEach(todo =>{
+          if(todo.completed == true)
+          todo.completed = false
+          else todo.completed = true;
+        })
 
       },
+
     },
-  }
-);
+  }); // Aplication Todo
